561f449f1d8395231f0d5e53d132198977a87b1f
Brian Lee <blee0613@gmail.com>
2020-04-20 16:36:41 -0700

    Updated game.js and index.html to support webpage with button functionality

--- game.js
+++ game.js
@@ -1,50 +1,57 @@
-
+let playerWins = 0;
+let computerWins = 0;
+const MAX_POINTS = 5;
+let game_over = false;
 
 function computerPlay() {
-    // randomly returns rock, paper, or scissors
     let moves = ["rock", "paper", "scissors"];
-    const randomElement = moves[Math.floor(Math.random() * moves.length)];
-    return randomElement;
-
+    return moves[Math.floor(Math.random() * moves.length)];
 }
 
-// here in case we need to test in VSC, prompt() not supported
-//function playerPlay() {
-    //let moves = ["rock", "paper", "scissors"];
-    //const randomElement = moves[Math.floor(Math.random() * moves.length)];
-    //return randomElement;
+function game() {
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+        if (!game_over) playRound(button.id);
+    });
+});
+}
 
-//}
-
-function playRound(playerSelection, computerSelection) {
-    let playerMove = playerSelection.toLowerCase();
+function playRound(playerMove) {
+    let computerSelection = computerPlay();
+    let currentResult = document.querySelector('#roundResults');
 
     if (playerMove === computerSelection) {
-    console.log(`Tie, you both picked ${playerSelection}!`)
-    return "Tie";
+    currentResult.textContent = `Tie, you both picked ${playerMove}!`;
+    updateStandings("tie");
+    return;
     }
 
     if (playerMove === "rock") {
         switch (true) {
             case computerSelection === "paper":
-                console.log("You lose, paper beats rock!");
-                return "computer";
+                currentResult.textContent = `You lose, paper beats rock!`;
+                updateStandings("computer");
+                break;
 
             case computerSelection === "scissors":
-                console.log("You win, rock beats scissors!");
-                return "player";
+                currentResult.textContent = `You win, rock beats scissors!`;
+                updateStandings("player");
+                break;
         }
     }
 
     if (playerMove === "paper") {
         switch (true) {
             case computerSelection === "scissors":
-                console.log("You lose, scissors beats paper!");
-                return "computer";
+                currentResult.textContent = `You lose, scissors beats paper!`;
+                updateStandings("computer");
+                break;
 
             case computerSelection === "rock":
-                console.log("You win, paper beats rock!");
-                return "player";
+                currentResult.textContent = `You win, paper beats rock!`;
+                updateStandings("player");
+                break;
         }
     }
     
@@ -52,44 +59,48 @@ function playRound(playerSelection, computerSelection) {
     if (playerMove === "scissors") {
         switch (true) {
             case computerSelection === "rock":
-                console.log("You lose, rock beats scissors!");
-                return "computer";
+                currentResult.textContent = `You lose, rock beats scissors!`;
+                updateStandings("computer");
+                break;
 
             case computerSelection === "paper":
-                console.log("You win, scissors beats paper!");
-                return "player";
+                currentResult.textContent = `You win, scissors beats paper!`;
+                updateStandings("player");
+                break;
         }
     }
 
 
 }
 
-function game() {
-    console.log("Welcome to Rock, Paper, Scissors! This will be a best-of-5.")
-    let playerWins = 0;
-    let computerWins = 0;
+function updateStandings(winner) {
+    let scoreBoard = document.querySelector('#score');
 
-    while (playerWins < 3 && computerWins < 3) {
-        const playerMove = prompt("Please enter your move");
-        // const playerMove = playerPlay();
-        // here in case we need to test in VSC (prompt() not supported in node.js)
-        const computerMove = computerPlay();
-        if (playRound(playerMove, computerMove) === "player") {
-            console.log("You won this round!");
-            playerWins++;
-        } else if (playRound(playerMove, computerMove) === "computer") {
-            console.log("You lost this round :(");
-            computerWins++;
-        } else {
-            console.log("tie");
-        }
-        console.log(`The score so far is: Player: ${playerWins}, Computer: ${computerWins}`)
-    }
+    if (winner == "computer") computerWins++;
+    if (winner == "player") playerWins++;
 
-    if (playerWins === 3) {
-        console.log("Congrats, you win!");
-    } else {
-        console.log("Sorry, you lost :(");
-    }
+    scoreBoard.textContent = `Player: ${playerWins}, Computer: ${computerWins}`;
+    whoWon();
+
 
 }
+
+function whoWon() {
+    let scoreBoard = document.querySelector('#score');
+    if (computerWins == MAX_POINTS) {
+        scoreBoard.textContent = `You Lose :( \n Final Score is Player: ${playerWins}, Computer: ${computerWins}`;
+        gameOver();
+    }
+
+    if (playerWins == MAX_POINTS) {
+        scoreBoard.textContent = `You Win! \n Final Score is Player: ${playerWins}, Computer: ${computerWins}`;
+        gameOver();
+    }
+}
+
+function gameOver() {
+    game_over = true;
+}
+    
+
+game();
